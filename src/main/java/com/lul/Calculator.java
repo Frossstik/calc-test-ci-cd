97a1e5e4ec7864e7659a5c14571ecba10e752250
Frossstik <lehapoh@gmail.com>
2023-05-18 04:42:32 +0300

    test ignore

--- src/main/java/com/lul/Calculator.java
+++ src/main/java/com/lul/Calculator.java
@@ -3,17 +3,17 @@ package com.lul;
 public class Calculator {
     public double sum(double a, double b){
         return a + b;
-    }
+    }// +
 
     public double sub(double a, double b){
         return a - b;
-    }
+    }// -
 
     public double mult(double a, double b){
         return a*b;
-    }
+    }// *
 
     public double div(double a, double b){
         return a/b;
-    }
+    }// /
 }
